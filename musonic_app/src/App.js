c95f83eedc935270844ff25944ecf83143215421
Akshay Sahu <akki5233@gmail.com>
2021-05-16 11:07:30 +0530

    Login page created

---
+++ musonic_app/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Login from './Login';
+
+
+function App() {
+  return (
+    <div className="app">
+      {/* <h1>APP</h1> */}
+      <Login />
+    </div>
+  );
+}
+
+export default App;
