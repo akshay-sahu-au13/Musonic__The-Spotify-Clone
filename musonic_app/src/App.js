c33cfd2e20a29b10ca4e019b50170200ef1acba4
Akshay Sahu <akki5233@gmail.com>
2021-05-23 01:13:42 +0530

    Connected to Spotify web api using spotify-web-api-js package

--- musonic_app/src/App.js
+++ musonic_app/src/App.js
@@ -1,20 +1,48 @@
 // import logo from './logo.svg';
+// install spotify web api: npm install spotify-web-api-js
+
 import './App.css';
 import Login from './Login';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getTokenFromUrl } from './musonic';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
 
+  const [token, setToken] = useState(null);
+
   useEffect(() => {
-    const token = getTokenFromUrl();
-    console.log("I HAVE A TOKEN  >>> ", token);
+    const hash = getTokenFromUrl();
+    console.log("I HAVE A TOKEN 👉👉 >>> ", hash);
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        console.log("👨", user);
+      })
+    }
+
+    
+
   }, [])
+
+  
 
   return (
     <div className="app">
-      {/* <h1>APP</h1> */}
-      <Login />
+      {
+        token ?
+          (<h1>Logged in to spotify</h1>)
+          : (
+            <Login />
+          )
+      }
+      
     </div>
   );
 }
