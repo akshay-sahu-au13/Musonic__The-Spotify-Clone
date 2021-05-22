e1b565defa450896349bf1038b3bad0c8813ad67
Akshay Sahu <akki5233@gmail.com>
2021-05-22 23:47:23 +0530

    Adding login feeatures

--- musonic_app/src/musonic.js
+++ musonic_app/src/musonic.js
@@ -18,3 +18,15 @@ const scopes = [
 
 // Login url to authenticate user via spotify api
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
+
+export const getTokenFromUrl = () => {
+    return window.location.hash
+            .substring(1)
+            .split("&")
+            .reduce((initial, item) => {
+                var parts = item.split('=');
+                initial[parts[0]] = decodeURIComponent(parts[1])
+                return initial
+            },{})
+
+}
