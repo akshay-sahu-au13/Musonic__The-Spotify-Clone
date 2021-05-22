4c90c4468b20beb8c1ec4cc61be76af761e7f029
Akshay Sahu <akki5233@gmail.com>
2021-05-23 00:06:42 +0530

    Decoding the auth token from spotify

--- musonic_app/src/App.js
+++ musonic_app/src/App.js
@@ -1,9 +1,16 @@
 // import logo from './logo.svg';
 import './App.css';
 import Login from './Login';
-
+import { useEffect } from 'react';
+import { getTokenFromUrl } from './musonic';
 
 function App() {
+
+  useEffect(() => {
+    const token = getTokenFromUrl();
+    console.log("I HAVE A TOKEN  >>> ", token);
+  }, [])
+
   return (
     <div className="app">
       {/* <h1>APP</h1> */}
